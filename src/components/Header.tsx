28c2d0c5b8c5612ae0800d81bef4aa5ccd7e32e1
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2025-07-07 01:19:03 +0000

    fix: add CI, nvm, rem unused imports and add missing types

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-
 export default function Header({ title }: { title: string }) {
   return (
     <div className="bg-[#1e3a5f] text-white">
       <div className="bg-[#172b42] px-6 py-4">
-        <div className="container mx-auto max-w-6xl flex items-center gap-4">
-        </div>
+        <div className="container mx-auto max-w-6xl flex items-center gap-4" />
       </div>
       <div className="px-6 py-12">
         <h1 className="text-4xl font-bold container mx-auto max-w-6xl">{title}</h1>
